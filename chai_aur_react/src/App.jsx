e1cb1e9c97d1c4cdd11d6da10acb2ab0c0d32cf6
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-01-17 23:49:57 +0530

    ,

--- chai_aur_react/src/App.jsx
+++ chai_aur_react/src/App.jsx
@@ -1,8 +1,13 @@
 function App() {
+  let orders = 5;
+  function additionfunc() {
+    orders = orders + 1;
+    console.log(orders);
+  }
   return (
     <>
       <h1>Chai counter : {orders}</h1>
-      <button>add chai</button>
+      <button onClick={additionfunc}>add chai</button>
       <br />
       <button>remove chai</button>
     </>
