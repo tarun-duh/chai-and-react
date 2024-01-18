31392fc8cf9ca62a953a730f85f5984587c21c4a
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-01-18 21:14:56 +0530

    .

--- chai_aur_react/src/App.jsx
+++ chai_aur_react/src/App.jsx
@@ -1,15 +1,12 @@
+import { useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
 function App() {
-  let orders = 5;
-  function additionfunc() {
-    orders = orders + 1;
-    console.log(orders);
-  }
   return (
     <>
-      <h1>Chai counter : {orders}</h1>
-      <button onClick={additionfunc}>add chai</button>
-      <br />
-      <button>remove chai</button>
+      <h1 bg-red>tarun singh</h1>
     </>
   );
 }
